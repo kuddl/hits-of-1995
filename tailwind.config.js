2c5f4ffcc7bdf196c24711db6872f23e58107b97
Tobias Sailer <tobias.sailer@gmail.com>
2025-01-19 00:28:58 +0100

    change fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        headline: ["Cinzel", "serif"],
+        body: ["Fauna One", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
