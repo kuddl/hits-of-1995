7a977c8a031213fd16d6066b1a1135fcab0091a8
Tobias Sailer <tobias.sailer@gmail.com>
2025-05-01 16:34:10 +0200

    adasd (#1)

--- src/components/MyHeader.tsx
+++ src/components/MyHeader.tsx
@@ -1,4 +1,4 @@
-import { Disc3, Heart, Mic2, Music2, Radio } from "lucide-react";
+import { Disc3, Heart, MapPlus, MapPlusIcon, Mic2, Music2, Radio } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export const MyHeader = () => {
@@ -21,16 +21,24 @@ export const MyHeader = () => {
 
   return (
     <header className="relative mb-8 pt-16 text-center">
-      <div className="animate-float absolute inset-x-0 top-4 flex flex-wrap justify-center gap-12">
-        {headerIcons.map((icon, index) => (
-          <div
-            key={index}
-            className={`transform transition-transform duration-500 ${bounceIcon === index ? "-translate-y-2 scale-125" : "scale-100"
-              }`}
-          >
-            {icon}
-          </div>
-        ))}
+      <div className="flex items-center justify-between">
+        <div className="animate-float  left-4 top-4 flex flex-wrap gap-8  items-center justify-center ">
+          {headerIcons.map((icon, index) => (
+            <div
+              key={index}
+              className={`transform transition-transform duration-500 ${bounceIcon === index ? "-translate-y-2 scale-125" : "scale-100"
+                }`}
+            >
+              {icon}
+            </div>
+          ))}
+        </div>
+        <div className="right-4 top-4 shrink-0s">
+          <a href="https://schulweg-of-1995.netlify.app/" className="flex items-center gap-2 text-sm font-bold">
+            <MapPlusIcon className="inline text-pink-400 " />
+            Schulweg of 1995
+          </a>
+        </div>
       </div>
       <h1 className="mb-8 pt-20 text-7xl font-black leading-none md:text-[12rem]">
         <span className="animate-gradient-x inline-block bg-gradient-to-r from-pink-400 via-purple-400 to-cyan-400 bg-clip-text text-transparent drop-shadow-[0_0_25px_rgba(219,39,119,0.2)]">
@@ -67,7 +75,7 @@ export const MyHeader = () => {
       <p className="mx-auto mt-2 max-w-2xl text-lg text-white/80">
         Viel Spaß beim Entdecken und Erinnern!
       </p>
-     
+
     </header>
   );
 };
